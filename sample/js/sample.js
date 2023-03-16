941264d49e11113bb87eb501280a4cc2fe4b3c78
KanCheng <zxdfgcv@gmail.com>
2023-03-16 23:27:11 +0800

    Added testing.

--- sample/js/sample.js
+++ sample/js/sample.js
@@ -6,9 +6,12 @@ const puppeteer = require('puppeteer-core');
     executablePath: // 對應本地端的 Chrome 位置
     // Mac
       //'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-      'C:/Program Files/Google/Chrome/Application/chrome.exe'
+      'C:/Program Files/Google/Chrome/Application/chrome.exe',
     // Windows 可對應 Chrome .exe 檔案
-    headless: false // 無外殼的 Chrome，有更佳的效能
+    // headless: false 
+    // 無外殼的 Chrome，有更佳的效能
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage(); // 開啟新分頁
   // await page.goto('https://example.com');
@@ -17,6 +20,11 @@ const puppeteer = require('puppeteer-core');
   // await page.screenshot({ path: 'example.png' }); 
   await page.screenshot({ path: 'example.png' }); 
   // 截圖，並且存在...
-
   await browser.close(); // 關閉瀏覽器
 })();
+
+//browser = await puppeteer.launch({ 
+//  executablePath: 'chrome.exe', 
+//  headless: true,
+//  args: ['--no-sandbox', '--disable-setuid-sandbox']
+//});
